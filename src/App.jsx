6bdef8e04b23750b15c10c5b760bb38b9d4b8938
sagvel <saulko.oleksander@gmail.com>
2022-09-04 15:22:01 +0300

    add html layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>App</div>;
+};
 
-export default App
+export default App;
+
+
+// Algorithm
+// 1. Сформировать структуру компонентов
+// 2. Подключить Redux к проекту и определить начальное состояние
+// 3. Прописать логику запросов на api и записать данные в state
+// 4. Реализовать переключение между Departed/Arrival
+// ...
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.scss';
+import './styles/index.scss';
 const e = React.createElement;
 const rootElem = document.querySelector('#root');
 
