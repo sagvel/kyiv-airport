fe93d90f1cb12045a741f768d4c0992bfb2472ef
sagvel <saulko.oleksander@gmail.com>
2022-09-04 16:50:02 +0300

    add render data from server

--- src/features/flights/components/Flights.jsx
+++ src/features/flights/components/Flights.jsx
@@ -29,37 +29,135 @@ const Flights = ({ flightDataFetching, flights }) => {
 
   return (
     <>
-      <h1>Search flight</h1>
+      <h1 className="title">Search flight</h1>
       <SearchFlights />
-      <div className="buttons">
-        <button>Departures</button>
-        <button>Arrivals</button>
-      </div>
-      <table className="table">
-        <thead>
-          <tr>
-            <td>Terminal</td>
-            <td>Local time</td>
-            <td>Destination</td>
-            <td>Status</td>
-            <td>Airline</td>
-            <td>Flight</td>
-          </tr>
-        </thead>
-        <tbody>
-          {body &&
-            flights.body.departure.map(flight => (
+      <div className="search-results">
+        <div className="tabs">
+          <ul className="nav nav-tabs">
+            <li className="nav-tabs__item nav-tabs__item_active">
+              <a href="#" className="nav-tabs__link nav-tabs__link-departures">
+                <i>
+                  <svg
+                    data-v-7746f986=""
+                    width="40px"
+                    height="28px"
+                    viewBox="0 0 40 28"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g
+                      data-v-7746f986=""
+                      id="Page-1"
+                      stroke="none"
+                      strokeWidth="1"
+                      fill="none"
+                      fillRule="evenodd"
+                    >
+                      <g
+                        data-v-7746f986=""
+                        id="Path-403"
+                        transform="translate(-1.000000, -1.000000)"
+                        fill="#FFFFFF"
+                        fillRule="nonzero"
+                      >
+                        <g data-v-7746f986="" id="Group">
+                          <path
+                            data-v-7746f986=""
+                            d="M40.9190312,14.2925525 C40.731055,13.5210761 40.2396434,12.8581323 39.5561585,12.4539552 C38.8726735,12.0497781 38.0549837,11.9385881 37.2883912,12.1455827 L26.8096581,15.0218819 L13.1945082,2 L9.38678826,3.03046545 L17.5564785,17.5510243 L7.75204986,20.2442408 L3.86129243,17.1278331 L1,17.9171897 L4.59162231,24.3200818 L6.11030829,27.0112974 L9.26373268,26.1409043 L19.7414654,23.2656055 L28.3253427,20.9165445 L38.8070772,18.0372439 C40.4055057,17.5625364 41.3397513,15.9060318 40.9190312,14.2925525 Z"
+                            id="Path_403"
+                            transform="translate(21.009879, 14.505649) rotate(-4.012171) translate(-21.009879, -14.505649) "
+                          ></path>
+                        </g>
+                      </g>
+                    </g>
+                  </svg>
+                </i>
+                Departures
+              </a>
+            </li>
+            <li className="nav-tabs__item">
+              <a href="#" className="nav-tabs__link nav-tabs__link-arrivals">
+                <i>
+                  <svg
+                    data-v-7746f986=""
+                    width="40px"
+                    height="28px"
+                    viewBox="0 0 40 28"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g
+                      data-v-7746f986=""
+                      id="Page-1"
+                      stroke="none"
+                      strokeWidth="1"
+                      fill="none"
+                      fillRule="evenodd"
+                    >
+                      <g data-v-7746f986="" id="Group" transform="translate(-4.000000, -7.000000)">
+                        <g data-v-7746f986="" id="Path-402"></g>
+                        <path
+                          data-v-7746f986=""
+                          d="M43.8940167,20.2848496 C43.7061583,19.5138567 43.2150547,18.8513283 42.531998,18.4474045 C41.8489414,18.0434806 41.031764,17.9323603 40.2656518,18.1392252 L29.793485,21.013722 L16.1868667,8 L12.3815328,9.02981973 L20.5461037,23.5412796 L10.7478188,26.2328084 L6.85949946,23.1183536 L4,23.9072155 L7.5893717,30.3060954 L9.10710602,32.9956246 L12.2585544,32.1257769 L22.7297214,29.2522799 L31.3082198,26.9046908 L41.7833862,24.0271945 C43.3808131,23.5527844 44.3144732,21.8973179 43.8940167,20.2848496 Z"
+                          id="Path_402"
+                          fill="#FFFFFF"
+                          fillRule="nonzero"
+                          transform="translate(23.997341, 20.497812) rotate(27.974730) translate(-23.997341, -20.497812) "
+                        ></path>
+                      </g>
+                    </g>
+                  </svg>
+                </i>
+                Arrivals
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="calendar">
+          <input type="date" />
+          <div className="datas">
+            <div className="prev-data">
+              <div className="data__num">02/09</div>
+              <div className="data__title">Yesterday</div>
+            </div>
+            <div className="current-data">
+              <div className="data__num">03/09</div>
+              <div className="data__title">Today</div>
+            </div>
+            <div className="next-data">
+              <div className="data__num">03/09</div>
+              <div className="data__title">Tomorrow</div>
+            </div>
+          </div>
+        </div>
+        <table className="result-table">
+          <thead>
+            <tr>
+              <th>Terninal</th>
+              <th>Local time</th>
+              <th>Destination</th>
+              <th>Status</th>
+              <th>Airline</th>
+              <th>Flight</th>
+            </tr>
+          </thead>
+          <tbody>
+            {body.departure.map(flight => (
               <tr key={flight.ID}>
                 <td>{flight.term}</td>
-                <td>{flight.timeBoard}</td>
+                <td>{flight.timeDepShedule}</td>
                 <td>{flight['airportToID.city_en']}</td>
                 <td></td>
-                <td></td>
-                <td></td>
+                <td>
+                  <img className="airline-logo" src={flight.airline.en.logoName} />
+                  {flight.airline.en.name}
+                </td>
+                <td>{flight.codeShareData[0].codeShare}</td>
               </tr>
             ))}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
--- src/features/flights/components/SearchFlights.jsx
+++ src/features/flights/components/SearchFlights.jsx
@@ -2,10 +2,43 @@ import React from 'react';
 
 const SearchFlights = () => {
   return (
-    <div className="search-bar">
-      <input className="search-bar__input" type="text" />
-      <button className="search-bar__button">search</button>
-    </div>
+    <form className="search-flights__form" name="searchFlightsForm">
+      <svg
+        className="search-icon"
+        data-v-547e829a=""
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="-512.053 29 44 43"
+      >
+        <defs data-v-547e829a="">
+          {/* <style data-v-547e829a="">
+          .cls-1 {
+            fill: none;
+          }
+
+          .cls-2 {
+            fill: #95989a;
+          }
+        </style> */}
+        </defs>
+        <g data-v-547e829a="" id="ic_search_white" transform="translate(-512.053 29)">
+          <rect data-v-547e829a="" id="rectangle" width="44" height="43" className="cls-1"></rect>
+          <path
+            data-v-547e829a=""
+            id="ic_search_white-2"
+            data-name="ic_search_white"
+            d="M317.717,70.708H316.25l-.55-.538a11.072,11.072,0,0,0,2.933-7.525,11.92,11.92,0,1,0-11.917,11.646,11.628,11.628,0,0,0,7.7-2.867l.55.538V73.4l9.167,8.958,2.75-2.687Zm-11,0a8.065,8.065,0,1,1,8.25-8.063A8.124,8.124,0,0,1,306.717,70.708Z"
+            transform="translate(-289.3 -45.625)"
+            className="cls-2"
+          ></path>
+        </g>
+      </svg>
+      <input
+        className="search-flights__input"
+        type="text"
+        placeholder="Airline, destination or flight #"
+      />
+      <button className="search-flights__btn">Search</button>
+    </form>
   );
 };
 
